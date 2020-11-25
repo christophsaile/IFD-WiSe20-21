d9c118bdc1e35e8fc8b6a6e48daeb4e01c479f63
Christoph Saile <christoph.saile@googlemail.com>
2020-11-25 09:59:28 +0100

    chore: refactor js

--- vui-prototype/main.js
+++ vui-prototype/main.js
@@ -1,108 +1,111 @@
-window.addEventListener('load', function () {
-  init();
-});
-
-function init() {
-  const artyom = new Artyom();
-  const commands = [
-    {
-      indexes: [
-        'kannst du mir sagen was heute alles ansteht',
-        'kannst du mich auf den aktuellen Stand bringen',
-      ],
-      action: function () {
-        const answer = 'sehr gerne, mit was soll ich beginnen';
-        artyom.say(answer);
-        createNewMessageBox(answer, 'userOutput');
-      },
+window.addEventListener('load', onLoad());
+const artyom = new Artyom();
+const commands = [
+  {
+    indexes: [
+      'kannst du mir sagen was heute alles ansteht',
+      'kannst du mich auf den aktuellen Stand bringen',
+    ],
+    action: function () {
+      const answer = 'sehr gerne, mit was soll ich beginnen';
+      artyom.say(answer);
+      createNewMessageBox(answer, 'userOutput');
     },
-    {
-      indexes: ['ich bin noch etwas müde'],
-      action: function () {
-        const answer = 'soll ich deine guten Morgen Playlist abspielen';
-        artyom.say(answer);
-        createNewMessageBox(answer, 'userOutput');
-      },
+  },
+  {
+    indexes: ['ich bin noch etwas müde'],
+    action: function () {
+      const answer = 'soll ich deine guten Morgen Playlist abspielen';
+      artyom.say(answer);
+      createNewMessageBox(answer, 'userOutput');
     },
-    {
-      indexes: ['welche Abgaben habe ich noch offen'],
-      action: function () {
-        const answer =
-          'Bis Mittwoch, den 11.11.2020, musst du noch die Aufgabe 4 in Interface Design erledigen. Soll ich dir die Aufgabenstellung vorlesen';
-        artyom.say(answer);
-        createNewMessageBox(answer, 'userOutput');
-      },
+  },
+  {
+    indexes: ['welche Abgaben habe ich noch offen'],
+    action: function () {
+      const answer =
+        'Bis Mittwoch, den 11.11.2020, musst du noch die Aufgabe 4 in Interface Design erledigen. Soll ich dir die Aufgabenstellung vorlesen';
+      artyom.say(answer);
+      createNewMessageBox(answer, 'userOutput');
     },
-    {
-      indexes: ['welche Vorlesungen habe ich heute'],
-      action: function () {
-        const answer =
-          'Du hast heute zwei Blöcke Vorlesung. Zuerst hast du von 09:45 - 11:15 Uhr Interface Design. Im Anschluss hast du noch von 11:15 - 13:15 Uhr die Veranstaltung Streaming Anwendungen';
-        artyom.say(answer);
-        createNewMessageBox(answer, 'userOutput');
-      },
+  },
+  {
+    indexes: ['welche Vorlesungen habe ich heute'],
+    action: function () {
+      const answer =
+        'Du hast heute zwei Blöcke Vorlesung. Zuerst hast du von 09:45 - 11:15 Uhr Interface Design. Im Anschluss hast du noch von 11:15 - 13:15 Uhr die Veranstaltung Streaming Anwendungen';
+      artyom.say(answer);
+      createNewMessageBox(answer, 'userOutput');
     },
-    {
-      indexes: ['habe ich neue Nachrichten bekommen'],
-      action: function () {
-        const answer =
-          'Du hast eine neue E-Mail erhalten. Soll ich dir den Absender und den Betreff vorlesen';
-        artyom.say(answer);
-        createNewMessageBox(answer, 'userOutput');
-      },
+  },
+  {
+    indexes: ['habe ich neue Nachrichten bekommen'],
+    action: function () {
+      const answer =
+        'Du hast eine neue E-Mail erhalten. Soll ich dir den Absender und den Betreff vorlesen';
+      artyom.say(answer);
+      createNewMessageBox(answer, 'userOutput');
     },
-  ];
-
+  },
+];
+function onLoad() {
   document.querySelector('.startButton').addEventListener('click', () => {
-    document
-      .querySelector('.startScreen')
-      .classList.remove('active', 'animate__animated', 'animate__fadeIn');
-    document
-      .querySelector('.conversation')
-      .classList.add('active', 'animate__animated', 'animate__fadeIn');
-    artyom.addCommands(commands);
-    function startContinuousArtyom() {
-      artyom.fatality();
-      setTimeout(function () {
-        artyom
-          .initialize({
-            lang: 'de-DE',
-            continuous: true,
-            listen: true,
-            interimResults: true,
-            debug: true,
-          })
-          .then(function () {
-            console.log('Ready!');
-          });
-      }, 250);
-    }
-    startContinuousArtyom();
-    const answer =
-      'Hey Benni, ich hoffe du hast gut schlafen. Wie geht es dir heute';
-    artyom.say(answer);
-    createNewMessageBox(answer, 'userOutput');
-    artyom.redirectRecognizedTextOutput(function (recognized, isFinal) {
-      if (isFinal) {
-        createNewMessageBox(recognized, 'userInput');
-      } else {
-        console.log(recognized);
-      }
-    });
+    startArtyom();
   });
-
   document.querySelector('.endButton').addEventListener('click', () => {
-    document
-      .querySelector('.startScreen')
-      .classList.add('active', 'animate__animated', 'animate__fadeIn');
-    document
-      .querySelector('.conversation')
-      .classList.remove('active', 'animate__animated', 'animate__fadeIn');
-    artyom.say('Bis bald Benni');
-    artyom.dontObey();
-    document.querySelector('.conversation__messages').innerHTML = '';
-    console.log('Closed!');
+    endArtyom();
   });
+}
+
+function startArtyom() {
+  document
+    .querySelector('.startScreen')
+    .classList.remove('active', 'animate__animated', 'animate__fadeIn');
+  document
+    .querySelector('.conversation')
+    .classList.add('active', 'animate__animated', 'animate__fadeIn');
+  artyom.addCommands(commands);
+  function startContinuousArtyom() {
+    artyom.fatality();
+    setTimeout(function () {
+      artyom
+        .initialize({
+          lang: 'de-DE',
+          continuous: true,
+          listen: true,
+          interimResults: true,
+          debug: true,
+        })
+        .then(function () {
+          console.log('Ready!');
+        });
+    }, 250);
+  }
+  startContinuousArtyom();
+  const answer =
+    'Hey Benni, ich hoffe du hast gut schlafen. Wie geht es dir heute';
+  artyom.say(answer);
+  createNewMessageBox(answer, 'userOutput');
+  artyom.redirectRecognizedTextOutput(function (recognized, isFinal) {
+    if (isFinal) {
+      createNewMessageBox(recognized, 'userInput');
+    } else {
+      console.log(recognized);
+    }
+  });
+}
+
+function endArtyom() {
+  document
+    .querySelector('.startScreen')
+    .classList.add('active', 'animate__animated', 'animate__fadeIn');
+  document
+    .querySelector('.conversation')
+    .classList.remove('active', 'animate__animated', 'animate__fadeIn');
+  artyom.say('Bis bald Benni');
+  artyom.dontObey();
+  document.querySelector('.conversation__messages').innerHTML = '';
+  console.log('Closed!');
 }
 
 function createNewMessageBox(text, typeOfInput) {
@@ -115,5 +118,7 @@ function createNewMessageBox(text, typeOfInput) {
   );
   creatElem.append(addText);
   document.querySelector('.conversation__messages').appendChild(creatElem);
-  document.querySelector('.conversation__messages').lastElementChild.scrollIntoView();
+  document
+    .querySelector('.conversation__messages')
+    .lastElementChild.scrollIntoView();
 }
